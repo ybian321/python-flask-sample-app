aed886e17c147c78d2e0756734091d8d098a47a2
Gavin Tu <freefox@gmail.com>
2021-04-27 21:30:00 +1200

    kotlin script to build docker image

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,28 +1,49 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.PullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.dockerSupport
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+import com.kotlin.v2019_2.buildTypes.DockerBuild
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
 
 version = "2020.2"
 
+
 project {
+
+    vcsRoot(VCSExample)
+
+
+    buildType(DockerBuildsteve)
+
 }
+
+object DockerBuildsteve : DockerBuild({
+    name = "docker build"
+
+    vcs {
+        root(VCSExample)
+    }
+
+
+    repository = "13.239.84.133:8082"
+    imageName = "python-flask-sample-app"
+    tag = "v1.0.0"
+    artusername = "admin"
+    artpassword = "credentialsJSON:4365953f-8420-45b5-bebe-924c1243cd0a"
+
+})
+
+
+object VCSExample : GitVcsRoot({
+    name = "VCSExample"
+    url = "https://github.com/freefox-do/ython-flask-sample-app.git"
+    branch = "refs/heads/master"
+    authMethod = password {
+        userName = "capnash"
+        password = "d5bbe75793d3b650338fee279857bb1e90e8ea28"
+    }
+})
