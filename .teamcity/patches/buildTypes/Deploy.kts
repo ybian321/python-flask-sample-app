e2888b9aae7c18520b343682818793369a269f63
gavin <gavin@TeamCity>
2021-04-27 21:41:51 +1200

    TeamCity change in 'Gavin / python-flask-sample-app' project: runners of 'Deploy' build configuration were updated

--- .teamcity/patches/buildTypes/Deploy.kts
+++ .teamcity/patches/buildTypes/Deploy.kts
@@ -2,6 +2,7 @@ package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.ui.*
 
 /*
@@ -12,5 +13,17 @@ in the root project, and delete the patch script.
 create(DslContext.projectId, BuildType({
     id("Deploy")
     name = "Deploy"
+
+    steps {
+        script {
+            name = "DeployToK8s"
+            scriptContent = """
+                helm repo add helm-demo https://freefox1.jfrog.io/artifactory/helm --username "your artifactory username>" --password "your artifactory password"
+                helm repo update
+                helm upgrade --install python-flask-sample-app helm-demo/python-flask-sample-app
+            """.trimIndent()
+            dockerImage = "freefox1.jfrog.io/docker-devops/deploytok8s:1.0.0"
+        }
+    }
 }))
 
